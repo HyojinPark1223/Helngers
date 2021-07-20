a0072dc9899acf13b47b8c481c23fd14fc1b3b6f
unknown <dlxotjde@gmail.com>
2021-07-20 16:37:49 +0900

    taesung helngers v0.2 Modi login

--- frontend/src/routes/Login.vue
+++ frontend/src/routes/Login.vue
@@ -1,54 +1,57 @@
 <template>
-  <div class="user" id="login">
-    <div class="form-wrap">
-      <h3>Login</h3>
-      <div class="input-with-label">
-        <label for="email">email</label>
+  <div class="background">
+    <div class="user" id="login">
+      <div class="form-wrap">
+        <Logo />
         <br>
-        <input v-model="email" id="email" placeholder="이메일을 입력하세요" type="text" />
-        <div clss="error-text"></div>
-      </div>
-      <br>
-      <div class="input-with-label">
-        <label for="password">비밀번호</label>
+        <div class="input-with-label">
+          <label for="email" style="margin-right: 20px;">이메일</label>
+        
+          <input v-model="email" id="email" placeholder="이메일을 입력하세요" type="text" />
+          <div clss="error-text"></div>
+        </div>
         <br>
-        <input v-model="password" id="password" placeholder="비밀번호를 입력하세요" type="password" />
-        <div clss="error-text"></div>
-      </div>
-      <br>
-      <button type="button">
-        로그인
-      </button>
+        <div class="input-with-label">
+          <label for="password" style="margin-right: 20px;">비밀번호</label>
+          <input v-model="password" id="password" placeholder="비밀번호를 입력하세요" type="password" />
+          <div clss="error-text"></div>
+        </div>
+        <br>
+        <div class="button-div">
+          <button class="button-form" type="button" style="margin-right: 20px;">
+            LOGIN
+          </button>
+          <button class="button-form" type="button">
+            <router-link to="/signup" class="button-form">SIGNUP</router-link>
+          </button>
+        </div>
 
-      <div class="sns-login">
-        <div class="text">
-          <p>SNS 간편 로그인</p>
-          <div class="bar"></div>
+        <div class="add-option">
+          <div class="wrap">
+            <p>비밀번호를 잊으셨나요?</p>
+          </div>
         </div>
-        <div>
-          <img src="https://pbs.twimg.com/profile_images/770139154898382848/ndFg-IDH.jpg" alt="" width="55" height="55">
-          <img src="https://image.rocketpunch.com/company/5466/naver_logo.png?s=400x400&t=inside" alt="" width="55" height="55">
-          <img src="https://blog.kakaocdn.net/dn/Sq4OD/btqzlkr13eD/dYwFnscXEA6YIOHckdPDDk/img.jpg" alt="" width="55" height="55">
-        </div>
-        <!-- <kakaoLogin :component="component" />
-        <GoogleLogin :component="component" /> -->
       </div>
+    </div>
+    <div class="sns-login">
 
-      <div class="add-option">
-        <div class="wrap">
-          <p>비밀번호를 잊으셨나요?</p>
-        </div>
-        <div class="wrap">
-          <p>아직 회원이 아니신가요?</p>
-          <router-link to="/signup" class="btn--text">가입하기</router-link>
-        </div>
+      <div>
+        <img src="https://pbs.twimg.com/profile_images/770139154898382848/ndFg-IDH.jpg" alt="" width="55" height="55">
+        <img src="https://image.rocketpunch.com/company/5466/naver_logo.png?s=400x400&t=inside" alt="" width="55" height="55">
+        <img src="https://blog.kakaocdn.net/dn/Sq4OD/btqzlkr13eD/dYwFnscXEA6YIOHckdPDDk/img.jpg" alt="" width="55" height="55">
       </div>
+      <!-- <kakaoLogin :component="component" />
+      <GoogleLogin :component="component" /> -->
     </div>
   </div>
 </template>
 
 <script>
+import Logo from '../components/Logo'
 export default {
+  components: {
+    Logo,
+  },
   data() {
     return {
       email: "",
@@ -60,10 +63,33 @@ export default {
 </script>
 
 <style scoped>
+.background {
+  background-color: rgb(255,219,89, .73);
+}
+.button-div {
+  display: flex;
+}
+.button-form {
+  background-color: rgb(255,219,89, .73);
+  border-radius: 10px;
+  border: 0;
+  outline: 0;
+  color: white;
+
+}
+#password, #email {
+  background-color: #c8c8c8;
+  border-radius: 10px;
+  border: 0;
+  outline: 0;
+  
+}
 .form-wrap {
-  width: 700px;
-  height: 500px;
-  border: 1px solid black;
+  background-color: white;
+  width: 500px;
+  height: 300px;
+  border: 0;
+  outline: 0;
   border-radius: 50px;
 
   display: flex;
@@ -72,6 +98,7 @@ export default {
   align-items: center;
 }
 .user {
+
   display: flex;
   justify-content: center;
 }
