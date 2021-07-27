b1e0314d9327505f476c994386df064e053c39ff
정규원 <radio959@naver.com>
2021-07-27 14:51:25 +0900

    add medal detail

--- backend/src/main/java/com/ssafy/hellingers/controller/MedalController.java
+++ backend/src/main/java/com/ssafy/hellingers/controller/MedalController.java
@@ -1,16 +1,16 @@
 package com.ssafy.hellingers.controller;
 
-import com.ssafy.hellingers.model.Challenge;
 import com.ssafy.hellingers.model.Medal;
-import com.ssafy.hellingers.repository.ChallengeRepository;
 import com.ssafy.hellingers.repository.MedalRepository;
 import com.ssafy.hellingers.service.MedalService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
+import java.util.Optional;
 
 @RestController
 @RequestMapping("medals")
@@ -32,10 +32,11 @@ public class MedalController {
         return medalService.selectAll();
     }
 
-//    @GetMapping("/{id}")
-//    public List<Medal> print5() {
-//
-//    }
+    @GetMapping("/{id}")
+    public Optional<Medal> printDetail(@PathVariable Long id) {
+        Optional<Medal> medal = medalService.selectOne(id);
+        return medal;
+    }
 
 
 
--- backend/src/main/java/com/ssafy/hellingers/service/MedalService.java
+++ backend/src/main/java/com/ssafy/hellingers/service/MedalService.java
@@ -1,12 +1,12 @@
 package com.ssafy.hellingers.service;
 
-import com.ssafy.hellingers.model.Challenge;
 import com.ssafy.hellingers.model.Medal;
 import com.ssafy.hellingers.repository.MedalRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class MedalService {
@@ -17,11 +17,17 @@ public class MedalService {
 
     public List<Medal> selectAll() {
         List<Medal> all = medalRepository.findAll();
-        System.out.println("JPA 호출!@");
+        System.out.println("selectAll JPA 호출!@");
         for(Medal a : all) {
             System.out.println(a.getTitle());
         }
         return all;
+    }
+
+    public Optional<Medal> selectOne(Long id) {
+        Optional<Medal> medal = medalRepository.findById(id);
+        System.out.println("selectOne JPA 호출!@");
+        return medal;
     }
 
 }
