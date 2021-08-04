093d00cddf7ff820e7d31e8c225e3525752c1c0a
정상근 <topgeun7913@naver.com>
2021-08-04 11:19:21 +0900

    toproot 1.8 Login, Modal audit

--- frontend/src/routes/Login.vue
+++ frontend/src/routes/Login.vue
@@ -1,52 +1,60 @@
 <template>
   <div class="background">
-    <div class="user" id="login">
-      <div class="for-sns" style="display: flex; flex-direction: column; margin-top: 5%; margin-left: 25%">
+    <div
+      class="user"
+      id="login">
+      <div
+        class="for-sns">
         <div class="form-wrap">
-          <Logo />
-          <br>
+          <Logo class="Logo" />
           <div class="input-with-label">
-            <label for="email" style="margin-right: 35px;">이메일</label>
-          
-            <input v-model="email" id="email" placeholder="이메일을 입력하세요" type="text" />
+            <label
+              for="email">이메일&nbsp;&nbsp;</label>
+            <input
+              v-model="email"
+              id="email"
+              placeholder="이메일을 입력하세요"
+              type="text" />
             <div clss="error-text"></div>
           </div>
-          <br>
           <div class="input-with-label">
-            <label for="password" style="margin-right: 20px;">비밀번호</label>
-            <input v-model="password" id="password" placeholder="비밀번호를 입력하세요" type="password" />
+            <label
+              for="password">비밀번호</label>
+            <input
+              v-model="password"
+              id="password"
+              placeholder="비밀번호를 입력하세요"
+              type="password" />
             <div clss="error-text"></div>
           </div>
-          <br>
           <div class="button-div">
-            <button class="button-form" type="button" style="margin-right: 20px;">
+            <button
+              class="btn btn-primary">
               LOGIN
             </button>
-            <button class="button-form" type="button">
-              <router-link to="/signup" class="button-form">SIGNUP</router-link>
+            <button
+              class="btn btn-secondary"
+              @click="toSignup">
+              SIGN UP
             </button>
           </div>
-
           <div class="add-option">
             <div class="wrap">
               <p>비밀번호를 잊으셨나요?</p>
             </div>
           </div>
         </div>
-        <div class="sns-login" style="display: flex; margin-left: auto; margin-top: 10px;">
-
-          <div style="margin-left: auto;">
-            <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/5/53/Google_%22G%22_Logo.svg/600px-Google_%22G%22_Logo.svg.png" alt="" width="55" height="55">
-          </div>
-          <div>
-            <img src="https://image.rocketpunch.com/company/5466/naver_logo.png?s=400x400&t=inside" alt="" width="55" height="55">
-          </div>
-          <div style="display: flex; margin-right: 50px;">
-            <img src="https://blog.kakaocdn.net/dn/Sq4OD/btqzlkr13eD/dYwFnscXEA6YIOHckdPDDk/img.jpg" alt="" width="55" height="55">
-          </div>
-          
-          <!-- <kakaoLogin :component="component" />
-          <GoogleLogin :component="component" /> -->
+        <div
+          class="sns-login">
+          <img
+            src="https://me2.kr/0wmie"
+            alt="google" />
+          <img
+            src="https://me2.kr/rh1k0"
+            alt="naver" />
+          <img
+            src="https://me2.kr/vxczh"
+            alt="kakao" />
         </div>
       </div>
     </div>
@@ -65,57 +73,87 @@ export default {
       email: "",
       password: "",
     }
+  },
+  methods: {
+    toSignup() {
+      this.$router.push('/signup')
+    }
   }
-
 }
 </script>
 
-<style scoped>
-.for-sns {
-  border: black 1px;
-  width: 750px;
-  height: 500px;
-}
+<style lang="scss" scoped>
 .background {
-  height: 800px;
-  background-color: rgb(255,219,89, .73);
-}
-.button-div {
-  display: flex;
-}
-.button-form {
-  background-color: rgb(255,219,89, .73);
-  border-radius: 10px;
-  border: 0;
-  outline: 0;
-  color: white;
-
-}
-#password, #email {
-  background-color: #c8c8c8;
-  border-radius: 10px;
-  border: 0;
-  outline: 0;
-  
-}
-.form-wrap {
-  background-color: white;
-  width: 700px;
-  height: 400px;
-  border: 0;
-  outline: 0;
-  border-radius: 50px;
-
-  display: flex;
-  flex-direction: column;
-  justify-content: center;
-  align-items: center;
-}
-.user {
-  display: flex;
-  flex-direction: column;
-  justify-content: center;
-}
+    height: 800px;
+    background-color: rgb(255,219,89, .73);
+    font-family: 'Do Hyeon', sans-serif;
+    .user {
+      display: flex;
+      flex-direction: column;
+      justify-content: center;
+      .for-sns {
+        border: black 1px;
+        width: 600px;
+        height: 500px;
+        display: flex; 
+        justify-content: center;
+        align-content: center;
+        flex-direction: column;
+        margin: 10% auto;
+        .form-wrap {
+          background-color: white;
+          width: 600px;
+          height: 400px;
+          border: 0;
+          outline: 0;
+          border-radius: 50px;
+          display: flex;
+          flex-direction: column;
+          justify-content: center;
+          align-items: center;
+          .Logo {
+            margin-bottom: 20px;
+          }
+          .input-with-label {
+            margin: 10px 0;
+            input {
+              margin-left: 15px;
+            }
+          }
+          #password, #email {
+            background-color: #f1e5e5;
+            border-radius: 10px;
+            border: 0;
+            outline: 0;
+          }
+          .button-div {
+            display: flex;
+            button {
+              margin: 10px 20px;
+              height: 32px;
+              font-size: 15px;
+            }
+          }
+          .add-option {
+            margin: 10px 0;
+            color: rgb(100, 100, 100);
+          }
+        }
+        .sns-login {
+          width: 600px;
+          display: flex; 
+          margin-top: 10px;
+          img {
+            width: 35px;
+            height: 35px;
+            &:first-child {
+              margin-left: auto;
+            }
+          }
+        }
+      }
+    }
+  }
 
 
 </style>
