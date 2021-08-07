57a41345dd8c743d32a76e588b6529a472f6abb3
정상근 <topgeun7913@naver.com>
2021-08-07 23:01:35 +0900

    toproot v2.1 badgenow carousel

--- frontend/src/components/MedalCard.vue
+++ frontend/src/components/MedalCard.vue
@@ -3,35 +3,149 @@
     <div v-if="modalChk">
       <MedalModal />
     </div>
-    <div class="medal-card">
-      <div class="box">
-        <div
-          class="our-services settings"
-          @click="showModal">
-          <div class="icon"> 
-            <img
-              src="https://t1.daumcdn.net/cfile/tistory/99DBCD4E5A82385D1B"
-              width="100"
-              height="100" /> 
-          </div>
-          <h4>물구나무</h4>
-          <p>8/10</p>
-        </div>
+    <section>
+      <div class="header">
+        <span class="material-icons">
+          face
+        </span>
+        <h3>헬린이</h3>
       </div>
-    </div>
+      <Carousel
+        :settings="settings"
+        :breakpoints="breakpoints"
+        class="carousel">
+        <Slide
+          v-for="slide in slides"
+          :key="slide"
+          class="slide">
+          <div class="carousel__item">
+          </div>
+          <div class="progress">
+            <div
+              class="progress-bar"
+              role="progressbar"
+              style="width: 50%;"
+              aria-valuenow="50"
+              aria-valuemin="0"
+              aria-valuemax="100">
+              50%
+            </div>
+          </div>
+        </Slide>
+        <template #addons>
+          <Navigation />
+        </template>
+      </Carousel>
+    </section>
+    <section>
+      <div class="header">
+        <span class="material-icons">
+          directions_run
+        </span>
+        <h3>헬고딩</h3>
+      </div>
+      <Carousel
+        :settings="settings"
+        :breakpoints="breakpoints"
+        class="carousel">
+        <Slide
+          v-for="slide in slides"
+          :key="slide"
+          class="slide">
+          <div class="carousel__item">
+          </div>
+          <div class="progress">
+            <div
+              class="progress-bar"
+              role="progressbar"
+              style="width: 50%;"
+              aria-valuenow="50"
+              aria-valuemin="0"
+              aria-valuemax="100">
+              50%
+            </div>
+          </div>
+        </Slide>
+        <template #addons>
+          <Navigation />
+        </template>
+      </Carousel>
+    </section>
+    <section>
+      <div class="header">
+        <span class="material-icons">
+          fitness_center
+        </span>
+        <h3>헬창</h3>
+      </div>
+      <Carousel
+        :settings="settings"
+        :breakpoints="breakpoints"
+        class="carousel">
+        <Slide
+          v-for="slide in slides"
+          :key="slide"
+          class="slide">
+          <div class="carousel__item">
+          </div>
+          <div class="progress">
+            <div
+              class="progress-bar"
+              role="progressbar"
+              style="width: 50%;"
+              aria-valuenow="50"
+              aria-valuemin="0"
+              aria-valuemax="100">
+              50%
+            </div>
+          </div>
+        </Slide>
+        <template #addons>
+          <Navigation />
+        </template>
+      </Carousel>
+    </section>
   </div>
 </template>
 <script>
 import MedalModal from './MedalModal'
+import { Carousel, Navigation, Slide } from 'vue3-carousel';
 
 export default {
 	name: 'MedalCard',
 	components: {
 		MedalModal,
+    Carousel,
+    Slide,
+    Navigation,
 	},
 	data() {
 		return {
-      modalChk: false
+      modalChk: false,
+      slides: 10,
+      isStatusOn: false,
+      settings: {
+        itemsToShow: 1,
+        snapAlign: 'center',
+      },
+      // breakpoints are mobile first
+      // any settings not specified will fallback to the carousel settings
+      breakpoints: {
+        // 700px and up
+        700: {
+          itemsToShow: 4,
+          snapAlign: 'center',
+        },
+        // 1024 and up
+        1024: {
+          itemsToShow: 5,
+          snapAlign: 'start',
+        },
+        1474: {
+          itemsToShow: 6,
+          snapAlign: 'start',
+        },
+      },
 		}
 	},
 	methods: {
@@ -42,43 +156,39 @@ export default {
 }
 </script>
 <style lang="scss" scoped>
-    .medal-card {
-        height: 60%;
-        width: 300px;
-        margin-right: 10px;
-        margin-left: 10px;
-        /* border: 3px black solid; */
-        .box {
-            position: relative;
-            width: 100%;
-            padding-right: 15px;
-            padding-left: 15px;
-            .our-services:hover h4,
-            .our-services:hover p {
-                color: #fff
-            }
-            .our-services {
-                margin-top: 30px;
-                padding-top: 40px;
-                padding-bottom: 30px;
-                padding: 0 60px;
-                min-height: 198px;
-                text-align: center;
-                border-radius: 30px;
-                background-color: #fff;
-                transition: all .4s ease-in-out;
-                box-shadow: 0 0 25px 0 rgba(20, 27, 202, .17);
-                .icon {
-                    margin-bottom: -10px;
-                    transform: translateY(-50%);
-                    text-align: center;
-                }
-            }
-            .settings:hover {
-                box-shadow: 0 0 25px 0 rgba(20, 27, 201, .05);
-                cursor: pointer;
-                background-image: linear-gradient(-45deg, #dbb023 0%, #e2cc85 100%)
-            }
-        }
+.material-icons {
+  font-size: 40px;
+}
+.header {
+  display: flex;
+  justify-content: flex-start;
+  align-content: center;
+  margin: 20px 0 0;
+  h3 {
+    margin: 5px 5px;
+  }
+}
+.carousel{
+  .slide {
+    margin: 20px 10px 40px;
+    display: block;
+    .carousel__item {
+      font-family: 'Do Hyeon', sans-serif;
+      height: 100px;
+      width: 100px;
+      border-radius: 50%;
+      background-color: $primary;
+      color: var(--carousel-color-white);
+      display: flex;
+      justify-content: center;
+      align-items: center;
+      margin: 0 10px;
+      position: relative;
     }
+    .progress {
+      width: 70px;
+      margin: 20px 0 20px 27px;
+    }
+  }
+}
 </style>
