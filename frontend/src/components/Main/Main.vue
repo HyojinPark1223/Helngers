8b56cd024b0e8e5498c87400f077268bc26eb265
정상근 <topgeun7913@naver.com>
2021-08-12 10:41:55 +0900

    toproot v2.3 home popup complete

--- frontend/src/components/Main/Main.vue
+++ frontend/src/components/Main/Main.vue
@@ -79,6 +79,13 @@
         </div>
       </div>
     </div>
+    <div id="to-top">
+      <div
+        class="material-icons"
+        @click="toTop">
+        arrow_upward
+      </div>
+    </div>
   </div>
 </template>
 
@@ -138,7 +145,10 @@ export default {
         main.setAttribute('style', '')
       }
       window.scrollTo(0, this.scrollY)
-    }
+    },
+    toTop() {
+      window.scrollTo(0,0)
+    },
   }
 }
 </script>
@@ -342,5 +352,21 @@ export default {
       }
     }
   }
+}
+#to-top {
+  position: fixed;
+  bottom:30px;
+  right: 30px;
+  z-index: 9;
+  width: 42px;
+  height: 42px;
+  background-color: #333;
+  color: #fff;
+  border: 2px solid #fff;
+  border-radius: 10px;
+  cursor: pointer;
+  display: flex;
+  justify-content: center;
+  align-items: center;
 }
 </style>
