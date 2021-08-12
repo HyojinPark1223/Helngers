ee0c4f899e18eb3410764c3abd68553d3d3a0053
정규원 <radio959@naver.com>
2021-08-12 10:18:09 +0900

    DB setting

--- backend/src/main/java/com/ssafy/hellingers/controller/ProfileController.java
+++ backend/src/main/java/com/ssafy/hellingers/controller/ProfileController.java
@@ -1,13 +1,19 @@
 //package com.ssafy.hellingers.controller;
 //
+//import com.ssafy.hellingers.model.Medal;
 //import com.ssafy.hellingers.service.UserService;
 //import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.http.HttpStatus;
+//import org.springframework.http.ResponseEntity;
+//import org.springframework.web.bind.annotation.DeleteMapping;
+//import org.springframework.web.bind.annotation.PathVariable;
 //import org.springframework.web.bind.annotation.RequestMapping;
 //import org.springframework.web.bind.annotation.RestController;
 //
+//import java.util.Optional;
+//
 //@RestController
-////따라서 엔드포인트에 도달하려면 api/user/**가 될 수 있습니다.
-//@RequestMapping("/")
+//@RequestMapping("/profile")
 //public class ProfileController {
 //
 //    @Autowired
@@ -19,5 +25,15 @@
 //    // 수정하기를 눌렀을 때의 데이터 (nickname, introduce, email)
 //
 //    // 삭제하기
+//    @DeleteMapping("/delete/{email}")
+//    public ResponseEntity<?> deleteProfile(@PathVariable String email) {
+//        try {
+//            userService.deleteUser(email);
+//            return new ResponseEntity<>("DELETE_COMPLETE", HttpStatus.OK);
+//        }catch (Exception e) {
+//            e.printStackTrace();
+//        }
+//
+//    }
 //
 //}
