91ec094df550bd17eda5ce54142e839e449a7d86
Hyojin Park <gywls9712@naver.com>
2021-08-20 03:18:04 +0900

    Helngers Modify README Feature 1-4

--- frontend/webpack.config.js
+++ frontend/webpack.config.js
@@ -21,8 +21,8 @@ module.exports = {
   // 결과물(번들)을 반환하는 설정
   output: {
     // 주석은 기본값!, `__dirname`은 현재 파일의 위치를 알려주는 NodeJS 전역 변수
-    // path: path.resolve(__dirname, 'dist'),
-    // filename: 'main.js',
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'main.js',
     clean: true,
   },
 
